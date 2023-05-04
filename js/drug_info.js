febc7322ed9baaabbdb80150f2d56f407066b86f
IsThatADeveloper <104576149+IsThatADeveloper@users.noreply.github.com>
2023-05-03 20:02:51 -0400

    Update drug_info.js

--- js/drug_info.js
+++ js/drug_info.js
@@ -1,14 +1,6 @@
 const drugs = [
         {
-            name: "Abilify",
-            othernames: ["Aripiprazole", "Abilitat", "Aripiprex"],
-            type: "Brand",
-            dosage: {
-                form: "Tablet",
-                strength: "10 mg",
-                frequency: "Once daily"
-            },
-            description: "a medication used to treat schizophrenia, bipolar disorder, and depression. It works by affecting the activity of certain neurotransmitters in the brain, including dopamine and serotonin. It is also sometimes used to treat irritability associated with autism spectrum disorder."
+                description: "a medication used to treat schizophrenia, bipolar disorder, and depression. It works by affecting the activity of certain neurotransmitters in the brain, including dopamine and serotonin. It is also sometimes used to treat irritability associated with autism spectrum disorder."
         }
         { 
                 name: "Adderall", 
